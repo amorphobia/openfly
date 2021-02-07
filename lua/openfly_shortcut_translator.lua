9eb9bade3284e9ccb30840a638c90365ad0fac6f
amorphobia <pengxuesong.cn@gmail.com>
2021-02-07 13:42:19 +0800

    修复注释错误; 修改直通车候选文字

--- lua/openfly_shortcut_translator.lua
+++ lua/openfly_shortcut_translator.lua
@@ -2,21 +2,21 @@ local common = require("openfly_common")
 
 local labels = {
   ["Windows"] = {
-    ["oav"] = '安装目录',
-    ["ocm"] = '命令提示符',
-    ["odn"] = '文件管理器',
-    ["oec"] = 'Excel',
-    ["ogj"] = 'Rime 用户目录',
-    ["oht"] = '画图',
-    ["ojs"] = '计算器',
-    ["owd"] = 'Word',
-    ["owt"] = 'Windows Terminal'
+    ["oav"] = "[安装目录]",
+    ["ocm"] = "[命令提示符]",
+    ["odn"] = "[文件管理器]",
+    ["oec"] = "[Excel]",
+    ["ogj"] = "[Rime 用户目录]",
+    ["oht"] = "[画图]",
+    ["ojs"] = "[计算器]",
+    ["owd"] = "[Word]",
+    ["owt"] = "[Windows Terminal]"
   },
   ["Darwin"] = {
-    ["ocm"] = '终端',
-    ["odn"] = '访达',
-    ["ogj"] = 'Rime 用户目录',
-    ["ojs"] = '计算器'
+    ["ocm"] = "[终端]",
+    ["odn"] = "[访达]",
+    ["ogj"] = "[Rime 用户目录]",
+    ["ojs"] = "[计算器]"
   }
 }
 
